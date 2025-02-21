6fe95862f98daf23d44bc7c38e5b2a67068509a7
Abjal Momin <afzalmomin818@gmail.com>
2025-02-21 21:10:32 +0530

    user checking added

--- back-end/authentication/index.js
+++ back-end/authentication/index.js
@@ -7,13 +7,76 @@ app.use(express.json());
 const users = [];
 
 function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let options = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
 
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        token += options[Math.floor(Math.random() * options.length)] 
-    }
-
+  let token = "";
+  for (let i = 0; i < 32; i++) {
+    token += options[Math.floor(Math.random() * options.length)];
+  }
+  return token;
 }
 
 app.post("/signup", function (req, res) {
@@ -21,19 +84,32 @@ app.post("/signup", function (req, res) {
   const password = req.body.password;
 
   users.push({
-    username: username;
-    password: password
-  })
+    username: username,
+    password: password,
+  });
 
   res.json({
-    message: "You have signed up"
-  })
-  
-
+    message: "You have signed up",
+  });
 });
 app.post("/signin", function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
+
+  let foundUser = null;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username == username && users[i].password == password) {
+      foundUser = users[i];
+    }
+
+    if (foundUser) {
+      const token = generateToken();
+      foundUser.token = token;
+    }
+    res.json({
+      message: token,
+    });
+  }
 });
 
 app.listen(3000);
