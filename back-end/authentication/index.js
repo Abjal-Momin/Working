40a9d17eaa3c35b75d78f27bdaa93047b7a0f3e4
Abjal Momin <afzalmomin818@gmail.com>
2025-02-22 12:26:38 +0530

    jwt is added first time

--- back-end/authentication/index.js
+++ back-end/authentication/index.js
@@ -1,82 +1,11 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "afzalmomin";
 
 const app = express();
 app.use(express.json());
 
 const users = [];
-
-function generateToken() {
-  let options = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ];
-
-  let token = "";
-  for (let i = 0; i < 32; i++) {
-    token += options[Math.floor(Math.random() * options.length)];
-  }
-  return token;
-}
 
 app.post("/signup", function (req, res) {
   const username = req.body.username;
@@ -90,7 +19,6 @@ app.post("/signup", function (req, res) {
   res.json({
     message: "You have signed up",
   });
-  console.log(users);
 });
 
 app.post("/signin", function (req, res) {
@@ -98,14 +26,20 @@ app.post("/signin", function (req, res) {
   const password = req.body.password;
 
   let foundUser = null;
+
   for (let i = 0; i < users.length; i++) {
     if (users[i].username == username && users[i].password == password) {
       foundUser = users[i];
     }
   }
+
   if (foundUser) {
-    const token = generateToken();
-    foundUser.token = token;
+    const token = jwt.sign(
+      {
+        username: foundUser.username,
+      },
+      JWT_SECRET
+    );
     res.json({
       token: token,
     });
@@ -115,17 +49,20 @@ app.post("/signin", function (req, res) {
     });
   }
   console.log(users);
-  //   console.log(foundUser);
 });
 
 app.get("/me", function (req, res) {
   const token = req.headers.token;
+  const decodedInformation = jwt.verify(token, JWT_SECRET);
+  const username = decodedInformation.username;
+
   let foundUser = null;
   for (let i = 0; i < users.length; i++) {
-    if (users[i].token == token) {
+    if (users[i].username == username) {
       foundUser = users[i];
     }
   }
+
   if (foundUser) {
     res.json({
       username: foundUser.username,
@@ -133,7 +70,7 @@ app.get("/me", function (req, res) {
     });
   } else
     res.status(401).send({
-      message: "invalid token",
+      message: "Invalid token",
     });
 });
 
